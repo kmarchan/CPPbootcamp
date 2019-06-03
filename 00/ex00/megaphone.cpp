4c9e7344f9ed61b08901f414963fd1817eb8169e
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-06-03 19:40:50 +0200

    * LOUD AND UNBEARABLE FEEDBACK NOISE *

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -10,7 +10,9 @@ void capitalizer (std::string in) {
 
 int main(int argc, char **argv)
 {
-    if (argc < 1){return 0; }
+    if (argc < 2){ 
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    }
     for(int i = 1; i < argc; i++) {
         capitalizer(argv[i]);
     }
