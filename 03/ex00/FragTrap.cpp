841a663d0f1a8b11a4b35dbaa54ff8d743946328
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-06-08 15:52:05 +0200

    add pdf 03 00 wip

---
+++ 03/ex00/FragTrap.cpp
@@ -0,0 +1,74 @@
+#include "FragTrap.hpp"
+
+void FragTrap::rangedAttack(std::string const &target) {
+	std::cout	<< this->name 
+				<< " throws something at "
+				<< target
+				<< " for "
+				<< this->rangedDamage
+				<< " damage"
+				<< std::endl;
+}
+
+void FragTrap::meleeAttack(std::string const &target) {
+	std::cout	<< this->name 
+				<< " slaps "
+				<< target
+				<< " for "
+				<< this->rangedDamage
+				<< std::endl;
+}
+
+void FragTrap::takeDamage(unsigned int amount) {
+	std::cout	<< this->name 
+				<< " takes "
+				<< amount
+				<< " damage"
+				<< std::endl;
+	if (amount - this->armorDamageReduction >= 0) {
+	this->hitPoints -= (amount - this->armorDamageReduction);
+	std::cout << "Health is now " << this->hitPoints << std::endl;
+	}
+}
+
+void FragTrap::beRepaired(unsigned int amount) {
+	std::cout	<< this->name 
+				<< " is healed for "
+				<< amount
+				<< std::endl;
+	if (this->hitPoints + amount <= this->maxHitPoints) {
+		this->hitPoints += amount;
+	}
+	else {this->hitPoints = this->maxHitPoints; }
+	std::cout << "Health is now " << this->hitPoints << std::endl;
+}
+
+void FragTrap::vaulthunter_dot_exe(std::string const & target) {
+	if (this->energyPoints < 25) {
+		std::cout << "Vault hunter has fallen asleep" << std::endl;
+	}
+	else {
+		std::cout << "Ramdom attack" << std::endl;
+		this->energyPoints -= 25;
+	}
+}
+
+FragTrap::FragTrap() {}
+
+FragTrap::FragTrap(std::string name){
+	std::cout << "A wild " << name << " appears" << std::endl;
+	this->name = name;
+	this->hitPoints = 100;
+	this->maxHitPoints = 100;
+	this->energyPoints = 100;
+	this->maxEnergyPoints = 100;
+	this->level = 1;
+	this->meleeDamage = 30;
+	this->rangedDamage = 20;
+	this->armorDamageReduction = 5;
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << this->name << " has been deconstructed" << std::endl;
+}
---
+++ 03/ex00/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "FragTrap.hpp"
+
+int main(int argc, char const *argv[])
+{
+    FragTrap ft("ClapTrap");
+
+    ft.takeDamage(40);
+    ft.vaulthunter_dot_exe("Badass");
+	ft.vaulthunter_dot_exe("Badass");
+	ft.vaulthunter_dot_exe("Badass");
+	ft.vaulthunter_dot_exe("Badass");
+	ft.vaulthunter_dot_exe("Badass");
+	ft.vaulthunter_dot_exe("Badass");
+	ft.vaulthunter_dot_exe("Badass");
+	ft.vaulthunter_dot_exe("Badass");
+	ft.vaulthunter_dot_exe("Badass");
+    ft.beRepaired(10);
+    ft.rangedAttack("Skag");
+    return 0;
+}
