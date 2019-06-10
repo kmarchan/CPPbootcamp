a80ee96dda2ced489838e3bd4ce7bcc3fcd8b2af
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-06-10 18:34:19 +0200

    wip

---
+++ 05/ex00/Bureaucrat.hpp
@@ -0,0 +1,34 @@
+#ifndef BUREAUCRAT_H
+# define BUREAUCRAT_H
+#include <iostream>
+
+class Bureaucrat
+{
+private:
+	const std::string name;
+	int grade;
+public:
+	std::string getName();
+	int getGrade();
+	Bureaucrat();
+	Bureaucrat();
+	~Bureaucrat();
+	class GradeTooHighException:public std::exception
+	{
+		private:
+			/* data */
+		public:
+			GradeTooHighException(/* args */);
+			~GradeTooHighException();
+	};
+	class GradeTooLowException:public std::exception
+	{
+		private:
+			/* data */
+		public:
+			GradeTooLowException(/* args */);
+			~GradeTooLowException();
+	};
+};
+
+#endif // !BUREAUCRAT_H
---
+++ 05/ex00/Bureucrat.cpp
@@ -0,0 +1,28 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(/* args */)
+{
+}
+
+Bureaucrat::Bureaucrat()
+{
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+		
+Bureaucrat::GradeTooHighException::GradeTooHighException(/* args */)
+{
+}
+
+Bureaucrat::GradeTooHighException::~GradeTooHighException()
+{
+}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException(/* args */)
+{
+}
+
+Bureaucrat::GradeTooLowException::~GradeTooLowException()
+{
