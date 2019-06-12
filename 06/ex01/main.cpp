cd79bf915d976ed1b459675c33692cdb4af15621
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-06-12 20:03:05 +0200

    started day 1

--- 06/ex01/main.cpp
+++ 06/ex01/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
-void *serialize(void) {
-
-}
-
-Data *deserialize(void *raw) {
-	
-}
-
 struct Data {
 	std::string s1;
 	std::string s2;
 	int n;
 };
+
+void *serialize(void) {
+
+}
+
+Data *deserialize(void *raw) {
+
+}
+
