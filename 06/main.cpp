aeef783fda05ffa263c62ad1647fead067750de6
Kirsten MARCHANT <kmarchan@c1r2s1.wethinkcode.co.za>
2019-06-12 12:16:09 +0200

    empty main 06

---
+++ 06/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cctype>
+
+int main(int argc, const char** argv) {
+     
+    if (argc != 2) {std::cout << "this program takes only one argument" << std::endl;}
+    in = argv[0];
+    return 0;
+} 
